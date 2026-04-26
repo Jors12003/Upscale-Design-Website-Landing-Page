c2e3b23cfc1586a606e1b1feb1e79c88ff40ee6a
jors <masagcadict@gmail.com>
2026-04-25 23:22:52 -0400

    before I further fucked up, lets make this a checkpoint so that I can revert anytime

--- script.js
+++ script.js
@@ -37,3 +37,38 @@ document.querySelectorAll('.slide-left, .slide-right, .slide-up').forEach((el) =
 });
 
 observer.observe(craftContent);
+
+const swiper = new Swiper('.testimonial-swiper', {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+/*hamburger menu*/
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navlinks');
+
+
+if (hamburger) {
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('open');
+        navLinks.querySelector('ul').classList.toggle('open');
+    });
+}
+
+    document.querySelectorAll('.navlinks a').forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('open');
+            navLinks.querySelector('ul').classList.remove('open');
+    });
+});
